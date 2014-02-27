5d25f913ae3406f0461e13b25cf7b130ee78bfdc
Andy Loughran <aloughran@scorecomms.com>
2014-02-27 23:00:20 +0000

    Included states/provinces data file

---
+++ download.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Run this command to get the zip files you'll need for manipulating the data
+
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_populated_places.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_map_subunits.zip
+http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces.zip
